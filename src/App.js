39051cbdd96a3f867a35b792c0591dbc803e0493
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-09-14 16:30:07 -0500

    create dice state and render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import './App.css';
 import Die from './Die';
 
 function App() {
 
+
+  /**
+ Map over the state numbers array to generate our array
+ * of Die elements and render those in place of our
+ * manually-written 10 Die elements.
+ */
+
+  const [dice, setDice] = React.useState(allNewDice())
+
   function randomDie() {
     return Math.floor(Math.random() * 6 + 1);
   }
+
+  const diceRender = dice.map(die => {
+    return <Die value={die} />
+  })
 
   function allNewDice() {
     let newDiceArr = [];
@@ -17,11 +31,11 @@ function App() {
     return newDiceArr
   }
 
-  console.log(allNewDice())
+  console.log(dice)
 
   return (
     <div className="game-board">
-      <Die value="1"/>
+      {diceRender}
       <button>Roll Dice</button>
     </div>
   );
